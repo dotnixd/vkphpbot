511d98f528aaf40092393e37e6e9713a8631dfec
dotnixd <dotnixd@gmail.com>
2020-12-21 11:30:34 +0300

    upd

--- commands/steve/main.php
+++ commands/steve/main.php
@@ -14,7 +14,7 @@ function run($vk, $data) {
 
             $steve = imagecreatefrompng("./blobs/steve.png");
             $src = imagecreatefromjpeg($filename);
-            $out = imagecreate(imagesx($src), imagesy($src) + 100);
+            $out = imagecreatetruecolor(imagesx($src), imagesy($src) + 100);
 
             imagecopy($out, $src, 0, 0, 0, 0, imagesx($src), imagesy($src));
             imagecopy($out, $steve, intval(imagesx($src) / 3), imagesy($out) - imagesy($steve), 0, 0, imagesx($steve), imagesy($steve));
