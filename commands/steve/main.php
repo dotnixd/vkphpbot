cbda81856aaf1229b16445d6e573302c6b5f14c7
dotnixd <dotnixd@gmail.com>
2020-12-21 11:14:29 +0300

    upd

---
+++ commands/steve/main.php
@@ -0,0 +1,33 @@
+джобс
+перенести вашу пикчу на презентацию Apple
+
+<?php
+
+require_once "./library/hash.php";
+
+function run($vk, $data) {
+    if($img = end($data->object->attachments)) {
+        if($img->type == "photo") {
+            $size = end($img->photo->sizes);
+            $filename = download($size->url, "jpg");
+
+            $steve = imagecreatefrompng("./blobs/steve.png");
+            $src = imagecreatefromjpeg($filename);
+            $out = imagecreate(imagesx($src), imagesy($src) + 100);
+
+            imagecopy($out, $src, 0, 0, 0, 0, imagesx($src), imagesy($src));
+            imagecopy($out, $steve, intval(imagesx($src) / 3), imagesy($out) - imagesy($steve), imagesx($steve), imagesy($steve));
+
+            imagepng($out, $filename);
+
+            $vk->sendImage($data->object->peer_id, $filename, ["message" => "= Ваша пикча ="]);
+
+            unlink($filename);
+            return;
+        }
+    }
+
+
+}
+
+?>
