c6060e625a7e6a21e8185ef37c427802ed1f84e7
dotnixd <dotnixd@gmail.com>
2020-12-21 15:56:25 +0300

    upd

--- commands/steve/main.php
+++ commands/steve/main.php
@@ -3,7 +3,6 @@
 
 <?php
 
-require_once "./library/hash.php";
 require_once "./library/download.php";
 
 function run($vk, $data) {
@@ -13,20 +12,19 @@ function run($vk, $data) {
             $filename = download($size->url, "jpg");
 
             $steve = imagecreatefrompng("./blobs/steve.png");
+            $out = imagecreatetruecolor(imagesx($steve), imagesy($steve));
             $src = imagecreatefromjpeg($filename);
-            $out = imagecreatetruecolor(1280, 770);
 
-            imagecopyresized($out, $src, 0, 0, 0, 0, imagesx($src), imagesy($src), 1280, 720);
-            imagecopyresized($out, $steve, intval(imagesx($src) / 3), imagesy($out) - imagesy($steve), 0, 0, imagesx($steve), imagesy($steve), imagesx($steve) - 70, imagesy($steve) - 70);
+            imagerotate($src, 7);
 
-            $hash = generateHash(40) . ".png";
+            imagecopyresized($out, $src, 228, 49, 0, 0, 1007, 508, imagesx($src), imagesy($src));
+            imagecopy($out, $steve, 0, 0, 0, 0, imagesx($steve), imagesy($steve));
 
-            imagepng($out, $hash);
+            imagepng($out, $filename);
 
             $vk->sendImage($data->object->peer_id, $hash, ["message" => "= Ваша пикча ="]);
 
             unlink($filename);
-            unlink($hash);
             return;
         }
     }
